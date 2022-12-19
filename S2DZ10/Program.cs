4b3e574106e71be1b51616d77c6de9fbd6a880d9
Ivan <118948995+kuzniatsouivan@users.noreply.github.com>
2022-12-20 01:00:36 +0300

    Сохранение

---
+++ S2DZ10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите 3-х значное число: ");
+int number = int.Parse(Console.ReadLine()!);
+if ((number >= 100) && (number < 1000))
+{
+    int firstD = number % 100;
+    int secondD = firstD / 10;
+    Console.Write(secondD);
+}
+else
+{
+    Console.Write("Введите 3-х значное число!");
+}
---
+++ S3DZ19/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите число из 5-ти чисел: ");
+int n = Math.Abs(int.Parse(Console.ReadLine()!));
+
+int[] array = new int[5];
+
+for (int i = 0; i <= 4; i++)
+{
+    array[i] = n % 10;
+    n = n / 10; 
+}
+if (array[0] == array[4] && array[1]== array[3])
+{
+    Console.WriteLine("Число является полиндроном");
+}
+else
+{
+    Console.WriteLine("Число неявляется полиндроном");
+}
+
+// Другой вариант решения:
+
+// Console.WriteLine("Введите число из 5-ти чисел: ");
+// string array = Console.ReadLine()!;
+
+// int i = 0;
+
+// if  (array[0] == '-' ) i = 1;
+
+// if (array[i] == array[4+ i ] && array[1 +i]== array[3 +i])
+// {
+//     Console.WriteLine("Число является полиндроном");
+// }
+// else
+// {
+//     Console.WriteLine("Число неявляется полиндроном");
+// }
