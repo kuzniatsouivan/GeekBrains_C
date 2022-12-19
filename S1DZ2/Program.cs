c11d13a9992f5ea4776dbfcc3cb9611ccaebf1db
Ivan <118948995+kuzniatsouivan@users.noreply.github.com>
2022-12-20 00:53:57 +0300

    Сохранение

---
+++ S1DZ2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+string imput1 = Console.ReadLine()!;
+int number1 = int.Parse(imput1);
+
+Console.Write("Введите второе число: ");
+string imput2 = Console.ReadLine()!;
+int number2 = int.Parse(imput2);
+
+if (number1 > number2)
+{
+    Console.Write($"Первое число {number1} больше чем второе: {number2}");
+}
+else if (number1 == number2)
+{
+    Console.Write($"Числа {number2} равны!");
+}
+else
+{
+Console.Write($"Второе число {number2} больше чем первое: {number1}");
+} 
---
+++ S1DZ4/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+string imput1 = Console.ReadLine()!;
+int number1 = int.Parse(imput1);
+
+Console.Write("Введите второе число: ");
+string imput2 = Console.ReadLine()!;
+int number2 = int.Parse(imput2);
+
+Console.Write("Введите третье число: ");
+string imput3 = Console.ReadLine()!;
+int number3 = int.Parse(imput3);
+
+if (number1 > number2)
+{
+
+    if (number1 > number3)
+    {
+        Console.Write($"Самое большое число {number1}");
+    }
+    else
+    {
+        Console.Write($"Самое большое число {number3}");
+    }
+}
+else
+{
+    if (number2 > number3)
+    {
+        Console.Write($"Самое большое число {number2}");
+    }
+    else
+    {
+        Console.Write($"Самое большое число {number3}");
+    }
+
+}
